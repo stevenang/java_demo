d824dfc386493f197b81aef82ea67c41b6ba2af3
Steven Ang <ang.steve@icloud.com>
2023-11-25 19:48:16 -0500

    Code update V10 - Update endpointOverride

--- modules/SqsEventLambdaDemo/src/main/java/tw/idv/stevenang/lambda/SqsEventLambdaDemo.java
+++ modules/SqsEventLambdaDemo/src/main/java/tw/idv/stevenang/lambda/SqsEventLambdaDemo.java
@@ -30,7 +30,8 @@ public class SqsEventLambdaDemo implements RequestHandler<SQSEvent, Void> {
         S3Configuration s3Configuration = S3Configuration.builder().pathStyleAccessEnabled(true).build();
         s3Client = S3Client.builder()
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-                .endpointOverride(URI.create("http://host.docker.internal:4566"))
+                .endpointOverride(URI.create("http://localhost.localstack.cloud:4566"))
+                //.endpointOverride(URI.create("http://host.docker.internal:4566"))
                 //.endpointOverride(URI.create("http://localhost:4566"))
                 //.endpointOverride(URI.create("http://awslocal:4566"))
                 .serviceConfiguration(s3Configuration)
